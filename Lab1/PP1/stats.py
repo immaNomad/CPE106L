3de1ff0635094e62848e883a05b819b617d04d37
Ross Abraham Morales <161103109+CoolFolks@users.noreply.github.com>
2024-04-25 13:53:11 +0800

    Update stats.py

--- Lab1/PP1/stats.py
+++ Lab1/PP1/stats.py
@@ -5,3 +5,12 @@ def mode(numbers):
     max_freq = max(frequency.values())
     modes = [num for num, freq in frequency.items() if freq == max_freq]
     return modes[0] if len(modes) == 1 else modes
+
+#Median Function
+def median(numbers):
+    numbers = sorted(numbers)
+    n = len(numbers)
+    if n % 2 == 1:
+        return numbers[n // 2]
+    else:
+        return (numbers[n // 2 - 1] + numbers[n // 2]) / 2
