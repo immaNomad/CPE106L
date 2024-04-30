62bfdfb112d52a4e19145969aa462c00a42446f0
GavenAnthonyTan <155783000+GavenAnthonyTan@users.noreply.github.com>
2024-04-30 22:57:39 +0800

    Create main.py

---
+++ Lab3/Problem2/main.py
@@ -0,0 +1,44 @@
+import random
+
+class Student:
+    def __init__(self, name, num_scores):
+        """Initializes a student with a name and a list of scores, all initially set to 0."""
+        self.name = name
+        self.scores = [0] * num_scores
+
+    def get_name(self):
+        """Returns the student's name."""
+        return self.name
+
+    def set_score(self, index, score):
+        """Sets the score at the given index (0-based)."""
+        self.scores[index] = score
+
+    def get_score(self, index):
+        """Returns the score at the given index (0-based)."""
+        return self.scores[index]
+
+    def get_average(self):
+        """Returns the average of the student's scores."""
+        return sum(self.scores) / len(self.scores)
+
+    def get_high_score(self):
+        """Returns the highest score of the student."""
+        return max(self.scores)
+
+    def __str__(self):
+        """Returns a string representation of the student."""
+        scores_str = " ".join(str(score) for score in self.scores)
+        return f"Name: {self.name}\nScores: {scores_str}"
+
+    def __eq__(self, other):
+        """Returns True if two students have the same name."""
+        return self.name == other.name
+
+    def __lt__(self, other):
+        """Returns True if the current student's name comes before the other student's name."""
+        return self.name < other.name
+
+    def __ge__(self, other):
+        """Returns True if the current student's name comes after or is equal to the other student's name."""
+        return self.name >= other.name
